cb553730f07dc1638d348f73ff89bdce00ef9bb3
Isabelle Smyth <smythi@bc.edu>
2019-11-03 15:23:17 -0500

    9.4 complete

--- Snacktacular/SpotDetailViewController.swift
+++ Snacktacular/SpotDetailViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import GooglePlaces
 class SpotDetailViewController: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
@@ -15,7 +15,7 @@ class SpotDetailViewController: UIViewController {
     @IBOutlet weak var averageRatingLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var tableView: UITableView!
-
+    
     var spot: Spot!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +26,13 @@ class SpotDetailViewController: UIViewController {
         nameField.text = spot.name
         addressField.text = spot.address
     }
-
+    
+    func updateUserInterface() {
+        nameField.text = spot.name
+        addressField.text = spot.address
+        
+    }
+    
     @IBAction func photoButtonPressed(_ sender: UIButton) {
     }
     
@@ -35,6 +41,14 @@ class SpotDetailViewController: UIViewController {
     
     @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
     }
+    
+    
+    @IBAction func lookupPlacePressed(_ sender: UIBarButtonItem) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
+    }
+    
     
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
         let isPresentingInAddMode = presentingViewController is UINavigationController
@@ -45,3 +59,38 @@ class SpotDetailViewController: UIViewController {
         }
     }
 }
+
+extension SpotDetailViewController: GMSAutocompleteViewControllerDelegate {
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place ID: \(place.placeID)")
+        print("Place attributions: \(place.attributions)")
+        spot.name = place.name!
+        spot.address = place.formattedAddress ?? ""
+        spot.coordinate = place.coordinate
+        dismiss(animated: true, completion: nil)
+        updateUserInterface()
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    func didRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func didUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+    }
+    
+}
+
