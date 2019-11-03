ec3fcd20dfad5644f9008f189732ba88c40cd14c
Isabelle Smyth <smythi@bc.edu>
2019-11-03 13:27:41 -0500

    Initial Commit

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,12 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Isabelle Smyth on 11/3/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+class Spots {
+    var spotsArray = [Spot]()
+}
